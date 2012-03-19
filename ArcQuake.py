e68112a7a213c324556dcbf71727de41838f89c7
Bryan Goodrich <goodrich.bryan@gmail.com>
2012-03-18 17:13:20 -0700

    added prototype script

---
+++ ArcQuake.py
@@ -0,0 +1,53 @@
+# import arcpy
+from urllib  import urlopen  # used to retrieve online information
+from xml.dom import minidom  # used to parse XML content
+
+
+
+# Establish user inputs here. Select appropriate url
+
+
+
+# Capture GeoRSS XML and isolate its 'item' elements
+#===== At this time, hardcoding the past hour RSS for prototyping =====
+url   = "http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M0.xml"
+doc   = minidom.parse(urlopen(url))       # XML document
+items = doc.getElementsByTagName("item")  # Want to avoid same-name elemtns in root
+
+
+
+# Initialize Python list elements for attributes
+lat   = []  # Latitude
+lng   = []  # Longitude
+depth = []  # Depth of quake (km)
+title = []  # Description of location
+mag   = []  # Magnitude of quake
+class = []  # Class of magnitude (integer)
+time  = []  # Timestamp of event
+
+
+
+# Populate list elements with appropriate components from GeoRSS
+for node in items.getElementsByTagName("geo:lat"):
+    lat.append(node.childNodes[0].data)
+
+for node in items.getElementsByTagName("geo:long"):
+    lng.append(node.childNodes[0].data)
+
+for node in items.getElementsByTagName("dc:subject"):
+    class.append(node.childNodes[0].data)
+
+for node in items.getElementsByTagName("dc:subject"):
+    depth.append(node.childNodes[2].data)  # 3rd dc:subject element in items
+
+for node in items.getElementsByTagName("pubDate"):
+    time.append(node.childNodes[0].data)
+
+for node in items.getElementsByTagName("title"):  # Title contains "M (mag), [title]"
+    m, t = node.childNodes[0].data.split(", ")    # Break Title into components
+    mag.append(m[2:])                             # Remove "M " from LHS
+    title.append(t)                               # Accept RHS as-is
+
+
+
+# Begin creating ArcGIS feature classes based on user inputs
