6dffc9a82100ee8fa093a31d49941bdd2eb8568d
bryangoodrich <goodrich.bryan@gmail.com>
2012-03-20 13:22:45 -0700

    Update ArcQuake.py

--- ArcQuake.py
+++ ArcQuake.py
@@ -1,9 +1,28 @@
+# ArcQuake.py -- Python-based ArcGIS tool to interface the USGS earthquake GeoRSS 
+# Author: Bryan Goodrich
+#         http://www.bryangoodrich.com
+# Date Created: 18 March 2012
+# Last Updated: 19 March 2012
+# -- Completely redesigned the RSS aggregation portion by creating the
+#    getData function and using list comprehension to simplify list
+#    processing. Also adapted the results into a final list of dictionary
+#    entries that name each of the GeoRSS item properties that get used
+#    later in creating ArcGIS geometry objects. 
+# -- Added the ArcGIS section by importing appropriate modules and 
+#    setting up a functioning framework that creates an empty 
+#    feature class and populates it. The ESRI Desktop help pages demonstrate
+#    how easy it is to access spatial references based on a user's installation
+#    at http://help.arcgis.com/en/arcgisdesktop/10.0/help/index.html#//000v000000p6000000
+# -- The next steps will require setting up the parameters to make this usable as
+#    an ArcMap/ArcCatalog script tool and then customizing validation and additional options.
+
+# ========== Define Imports and Library Functions ==========
 import arcpy, os
-from urllib  import urlopen # used to retrieve online information
-from xml.dom import minidom # used to parse XML content
-from arcpy   import env     # used to set workspace environments
-from arcpy.management import CreateFeatureclass
-from arcpy.management import AddField
+from urllib           import urlopen             # used to retrieve online information
+from xml.dom          import minidom             # used to parse XML content
+from arcpy            import env                 # used to set workspace settings
+from arcpy.management import CreateFeatureclass  # creates an empty feature class
+from arcpy.management import AddField            # adds fields to the feature class
 
 
 # ========== User-defined functions ==========
@@ -25,7 +44,7 @@ def getData(item):
         # This function returns a 2-element list containing title and
         # magnitude for the given quake.
         text = text.split(', ')
-        d = [reduce(lambda x, y: x + ', ' + y, text[1:])]
+        d    = [reduce(lambda x, y: x + ', ' + y, text[1:])]
         d.append(text[0][2:])
         return d
 
@@ -34,28 +53,28 @@ def getData(item):
     KEYS = ['full date', 'description', 'date', 'url', 'lat', 'long', 'class',
             'set', 'depth', 'guid']
             
-    nodeList = item.childNodes # Extract item's child nodes
-
     # This algorithm grabs the child nodes for each node in the node list.
     # Since every node is atomic, 'child' is a singular list. The values
     # are appended to the values list to create a dictionary.
     # However, changes are required to parse the 'description' into
     # magnitude and title components that it contains. See 'fracture' above.
-    x = [] # List to be populated with dictionary values
+    nodeList = item.childNodes                   # Extract item's child nodes
+    x = []                                       # List to be populated with dictionary values
     for child in [node.childNodes for node in nodeList]:
-        x.append(child[0].nodeValue)
-    x = dict(zip(KEYS, x)) # 'zip' pairs up keys and values
-    t, m = fracture(x['description'])
-    x['depth'] = x['depth'].split(' ')[0]  # keep only number portion; remove 'km'
-    x['title'] = t
+        x.append(child[0].nodeValue)             # Could use nested list comprehension, but ugh ...
+    x              = dict(zip(KEYS, x))          # 'zip' pairs up keys and values
+    t, m           = fracture(x['description'])  # Get title and magnitude portions
+    x['depth']     = x['depth'].split(' ')[0]    # keep only number portion; remove 'km'
+    x['title']     = t
     x['magnitude'] = m
     return x
+#  end function
 
 
 
-# Establish user inputs here. Select appropriate url
-# outpath = arcpy.GetParameterAsText(0) ...
-env.overwriteOutput = True
+# ========== Set Parameters and Environmental Variables ==========
+# opath = arcpy.GetParameterAsText(0) ...
+env.overwriteOutput = True  # Overwrite currently existing feature class
 
 
 # Capture GeoRSS XML and isolate its 'item' elements
@@ -67,16 +86,6 @@ doc   = minidom.parse(urlopen(url)) # XML document
 items = doc.getElementsByTagName("item") # Want to avoid same-name elements in root.
                                          # Returns list of 'item' document elements.
 
-
-
-# Populate list with dictionaries of feed item components.
-# All dictionary entries will be text.
-feed = []
-for item in items:
-    feed.append(getData(item))
-
-
-
 # Set projection for +proj=longlat +ellps=WGS84 +datum=WGS84
 prj     = "Coordinate Systems/Geographic Coordinate Systems/World/WGS 1984.prj"
 prjFile = os.path.join(arcpy.GetInstallInfo()["InstallDir"], prj)
@@ -84,6 +93,14 @@ opath   = "C:/temp/py/quake.shp"  # Hardcode location for prototyping
 
 
 
+# ========== Begin RSS Aggregation ==========
+feed = []
+for item in items:
+    feed.append(getData(item))
+
+
+
+# ========== Begin ArcGIS Representation ==========
 # Create feature class and add fields for additional Quake information
 CreateFeatureclass(os.path.dirname(opath), os.path.basename(opath), "Point",
                    spatial_reference = prjFile)
@@ -95,20 +112,23 @@ AddField(opath,  'location', ' TEXT')
 # -- not sure about date formats in ArcGIS yet. Work on this later.
                           
 
-
-# 
-cur = arcpy.InsertCursor(opath)                # Connect to empty Feature Class.
-for quake in feed:                             # Convert Python data to appropriate
-    lat            = float(quake['lat'])       # data types when stored in feature
-    lng            = float(quake['long'])      # class. 
-    pnt            = arcpy.Point(lng, lat) 
-    feat           = cur.newRow()
+# Connect to Feature Class and Populate Feature Table
+cur = arcpy.InsertCursor(opath)                # Connects to empty Feature Class
+for quake in feed:
+    lat            = float(quake['lat'])
+    lng            = float(quake['long']) 
+    pnt            = arcpy.Point(lng, lat)     # Create Point object
+    feat           = cur.newRow()              # Define a new Row object on cursor
     feat.shape     = arcpy.PointGeometry(pnt)  # Set geometry point class
-    feat.depth     = float(quake['depth'])
-    feat.magnitude = float(quake['magnitude'])
-    feat.mclass    = int(quake['class'])
-    feat.location  = quake['title']
-    cur.insertRow(feat)
+    feat.depth     = float(quake['depth'])     # Set Depth
+    feat.magnitude = float(quake['magnitude']) # Set Magnitude
+    feat.mclass    = int(quake['class'])       # Set Magnitude Class
+    feat.location  = quake['title']            # Set Location Description
+    cur.insertRow(feat)                        # Populate Feature Class record
     
 
+
 del cur  # Unlock table
+
+
+
